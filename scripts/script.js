7e7e9f9d70a2279f900064dbb91239d44f1e2360
Randula Yashasmith Mawaththa <85297495+Randula98@users.noreply.github.com>
2021-12-29 09:22:00 +0530

    initiate

---
+++ scripts/script.js
@@ -0,0 +1,40 @@
+
+function view(){
+    document.getElementById("view").style.visibility = "visible";
+    document.getElementById("add").style.visibility = "hidden";
+    document.getElementById("edit").style.visibility = "hidden";
+    document.getElementById("delete").style.visibility = "hidden";
+    document.getElementById("search").style.visibility = "hidden";
+}
+
+function add(){
+    document.getElementById("view").style.visibility = "hidden";
+    document.getElementById("add").style.visibility = "visible";
+    document.getElementById("edit").style.visibility = "hidden";
+    document.getElementById("delete").style.visibility = "hidden";
+    document.getElementById("search").style.visibility = "hidden";
+}
+
+function edit(){
+    document.getElementById("view").style.visibility = "hidden";
+    document.getElementById("add").style.visibility = "hidden";
+    document.getElementById("edit").style.visibility = "visible";
+    document.getElementById("delete").style.visibility = "hidden";
+    document.getElementById("search").style.visibility = "hidden";
+}
+
+function del(){
+    document.getElementById("view").style.visibility = "hidden";
+    document.getElementById("add").style.visibility = "hidden";
+    document.getElementById("edit").style.visibility = "hidden";
+    document.getElementById("delete").style.visibility = "visible";
+    document.getElementById("search").style.visibility = "hidden";
+}
+
+function search(){
+    document.getElementById("view").style.visibility = "hidden";
+    document.getElementById("add").style.visibility = "hidden";
+    document.getElementById("edit").style.visibility = "hidden";
+    document.getElementById("delete").style.visibility = "hidden";
+    document.getElementById("search").style.visibility = "visible";
+}
